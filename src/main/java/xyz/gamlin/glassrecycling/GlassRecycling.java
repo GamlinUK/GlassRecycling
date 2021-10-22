67499bbf7cb2bd03d76584fe950514e54125a1dd
Jamie Greatorex <jamie.greatorex99@gmail.com>
2021-10-22 10:13:03 +0100

    Project setup

---
+++ src/main/java/xyz/gamlin/glassrecycling/GlassRecycling.java
@@ -0,0 +1,17 @@
+package xyz.gamlin.glassrecycling;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlassRecycling extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
